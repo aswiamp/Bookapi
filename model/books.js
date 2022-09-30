dbed948d8305d61eaf57f2ac0ae70b32f8354a66
aswin <me@aswi.com>
2022-09-30 11:12:14 +0530

    initial commit

---
+++ model/books.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:[true,'please provide name']
+             },
+        Imageurl: {
+
+            
+            type:String
+
+        },
+        Author:{
+            type:String,
+            required:[true,'please provide author name']
+        },
+        pages:{
+            type:Number
+        },
+        price:{
+            type:Number,
+            required:[true,'price must be specified']
+        },
+        }  
+)
+module.exports=mongoose.model('book',bookSchema)
